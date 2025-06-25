d36a3201e4c160e4b3f085e9d8d918e44b1230c5
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2025-06-25 17:31:25 +0200

    Initial commit

---
+++ src/main/java/com/nicolaualfredo/voting/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nicolaualfredo.voting;
+
+/**
+ *
+ * @author nicolaualfredo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
