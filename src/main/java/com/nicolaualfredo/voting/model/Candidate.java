46a91c7af64556e2eac5e5b4c2b5707de0f08ec4
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2025-06-25 18:01:26 +0200

    feat: add project setup with pom.xml and create Candidate and Vote model classes

--- src/main/java/com/nicolaualfredo/voting/model/Candidate.java
+++ src/main/java/com/nicolaualfredo/voting/model/Candidate.java
@@ -9,5 +9,32 @@ package com.nicolaualfredo.voting.model;
  * @author nicolaualfredo
  */
 public class Candidate {
-    
+
+    private int id;
+    private String name;
+
+    public Candidate() {
+    }
+
+    public Candidate(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+
+    }
 }
--- src/main/java/com/nicolaualfredo/voting/model/Vote.java
+++ src/main/java/com/nicolaualfredo/voting/model/Vote.java
@@ -9,5 +9,31 @@ package com.nicolaualfredo.voting.model;
  * @author nicolaualfredo
  */
 public class Vote {
-    
+
+    private String voterId;  // identifica o eleitor, ex: CPF, RG, ou UUID
+    private int candidateId;
+
+    public Vote() {
+    }
+
+    public Vote(String voterId, int candidateId) {
+        this.voterId = voterId;
+        this.candidateId = candidateId;
+    }
+
+    public String getVoterId() {
+        return voterId;
+    }
+
+    public void setVoterId(String voterId) {
+        this.voterId = voterId;
+    }
+
+    public int getCandidateId() {
+        return candidateId;
+    }
+
+    public void setCandidateId(int candidateId) {
+        this.candidateId = candidateId;
+    }
 }
